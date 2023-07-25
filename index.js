f6b52eaaca0cd5f68183dae7d31f98a1440a56d6
mmmpartha <mpparthipan1998@gmail.com>
2023-07-25 19:37:50 +0530

    second_commit

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-function handleSubmit(){
+function handleSubmit() {
 
     const name = document.getElementById("name").value;
     const mobilenumber = document.getElementById("mobilenumber").value;
     const email = document.getElementById('email').value;
     const address = document.getElementById('address').value;
 
-    console.log("name set",mobilenumber);
-    sessionStorage.setItem("name",name);
-    sessionStorage.setItem("mobilenumber",mobilenumber)
-    sessionStorage.setItem("email",email)
-    sessionStorage.setItem("address",address)
+    var Details = [];
+    
+    var employeDetail = {
+        Name: name,
+        Mobilenumber: mobilenumber,
+        Email: email,
+        Address: address,
+        id: 0
+    };
+
+    console.log(name, mobilenumber, email, address);
+
+    if (name && mobilenumber && email && address) {
+        let id = employeDetail.length + 1;
+        console.log("employedetail", employeDetail);
+        Details.push(...employeDetail)
+        //    Detail.map((Detail)=>{
+        //     console.log("el",Detail);
+        //     localStorage.setItem("detail", JSON.stringify(Detail));
+        //    })
+        localStorage.setItem("detail", JSON.stringify(Details));
+        console.log("detail",JSON.stringify(Details));
+    }
+
+    // Regular expression pattern for email validation
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    // Clearing previous error messages
+    document.querySelectorAll(".form-group .error").forEach(field => field.classList.remove("error"));
+    document.querySelectorAll(".error-text").forEach(errorText => errorText.remove());
+
+    // Performing validation checks
+    // if (name === "") {
+    //     showError(fullnameInput, "Enter your full name");
+    // }
+    // if (!emailPattern.test(email)) {
+    //     showError(emailInput, "Enter a valid email address");
+    // }
+    // if (mobilenumber === "") {
+    //     showError(passwordInput, "Enter your mobilenumber");
+    // }
+    // if (address === "") {
+    //     showError(dateInput, "Enter your address");
+    // }
 
     //Mobile Number Validation
     var mobileNumber = document.getElementById("mobilenumber").value;
